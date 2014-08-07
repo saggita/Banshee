2a2d53b8f7185c37c8768bbd0f78b56ab230c843
Dmitry Kozlov <yozhijk@gmail.com>
2014-08-07 18:34:34 +0400

    Add some comments into the thread pool

--- FireRays/Banshee/async/thread_pool.h
+++ FireRays/Banshee/async/thread_pool.h
@@ -9,108 +9,122 @@
 #include <future>
 #include <iostream>
 
+///< An implementation of a concurrent queue
+///< which is providing the means to wait until 
+///< elements are available or use non-blocking
+///< try_pop method.
+///<
 template <typename T> class thread_safe_queue
 {
 public:
-	thread_safe_queue(){}
-	~thread_safe_queue(){}
+    thread_safe_queue(){}
+    ~thread_safe_queue(){}
 
-	void push(T const& t)
-	{
-		std::lock_guard<std::mutex> lock(mutex_);
-		queue_.push(t);
-		cv_.notify_one();
-	}
+    // Push element: one of the threads is going to 
+    // be woken up to process this if any
+    void push(T const& t)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        queue_.push(t);
+        cv_.notify_one();
+    }
 
-	void push(T&& t)
-	{
-		std::lock_guard<std::mutex> lock(mutex_);
-		queue_.push(std::move(t));
-		cv_.notify_one();
-	}
-	
-	void wait_and_pop(T& t)
-	{
-		std::unique_lock<std::mutex> lock(mutex_);
-		cv_.wait(lock, [this](){return !queue_.empty()});
-		t = queue_.front();
-		queue_.pop();
-	}
+    // Push element: one of the threads is going to 
+    // be woken up to process this if any
+    void push(T&& t)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        queue_.push(std::move(t));
+        cv_.notify_one();
+    }
 
-	bool try_pop(T& t)
-	{
-		std::lock_guard<std::mutex> lock(mutex_);
-		if (queue_.empty())
-			return false;
-		t = std::move(queue_.front());
-		queue_.pop();
-		return true;
-	}
+    // Wait until there are element to process.
+    void wait_and_pop(T& t)
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        cv_.wait(lock, [this](){return !queue_.empty()});
+        t = queue_.front();
+        queue_.pop();
+    }
 
-	
+    // Try to pop element. Returns true if element has been popped, 
+    // false if there are no element in the queue
+    bool try_pop(T& t)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        if (queue_.empty())
+            return false;
+        t = std::move(queue_.front());
+        queue_.pop();
+        return true;
+    }
 
 private:
-	std::mutex mutex_;
-	std::condition_variable cv_;
-	std::queue<T> queue_;
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    std::queue<T> queue_;
 };
 
 
-
+///< Thread pool implementation which is using concurrency
+///< available in the system
+///<
 template <typename RetType> class thread_pool
 {
 public:
+    thread_pool() 
+    {
+        done_ = false;
+        int num_threads = std::thread::hardware_concurrency();
+        num_threads = num_threads == 0 ? 2 : num_threads;
 
-	thread_pool() 
-	{
-		done_ = false;
-		int num_threads = std::thread::hardware_concurrency();
-		num_threads = num_threads == 0 ? 2 : num_threads;
-		
-		std::cout << num_threads << " hardware threads available\n";
-		
-		for (int i=0; i < num_threads; ++i)
-		{
-			threads_.push_back(std::thread(&thread_pool::run_loop, this));
-		}
-	}
+#ifdef _DEBUG
+        std::cout << num_threads << " hardware threads available\n";
+#endif
 
-	~thread_pool()
-	{
-		done_ = true;
-		std::for_each(threads_.begin(), threads_.end(), std::mem_fun_ref(&std::thread::join));
-	}
+        for (int i=0; i < num_threads; ++i)
+        {
+            threads_.push_back(std::thread(&thread_pool::run_loop, this));
+        }
+    }
 
-	std::future<RetType> submit(std::function<RetType()>&& f)
-	{
-		std::packaged_task<RetType()> task(std::move(f));
-		auto future = task.get_future();
-		work_queue_.push(std::move(task));
-		return future;
-	}
+    ~thread_pool()
+    {
+        done_ = true;
+        std::for_each(threads_.begin(), threads_.end(), std::mem_fun_ref(&std::thread::join));
+    }
+
+    // Submit a new task into the pool. Future is returned in
+    // order for caller to track the execution of the task
+    std::future<RetType> submit(std::function<RetType()>&& f)
+    {
+        std::packaged_task<RetType()> task(std::move(f));
+        auto future = task.get_future();
+        work_queue_.push(std::move(task));
+        return future;
+    }
 
 private:
-	void run_loop()
-	{
-		std::packaged_task<RetType()> f;
-		while(!done_)
-		{
-			if (work_queue_.try_pop(f))
-			{
-				f();
-			}
-			else
-			{
-				std::this_thread::yield();
-				std::this_thread::sleep_for(std::chrono::milliseconds(50));
-			}
-		}
-	}
+    void run_loop()
+    {
+        std::packaged_task<RetType()> f;
+        while(!done_)
+        {
+            if (work_queue_.try_pop(f))
+            {
+                f();
+            }
+            else
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            }
+        }
+    }
 
 
-	thread_safe_queue<std::packaged_task<RetType()> > work_queue_;
-	std::atomic_bool done_;
-	std::vector<std::thread> threads_;
+    thread_safe_queue<std::packaged_task<RetType()> > work_queue_;
+    std::atomic_bool done_;
+    std::vector<std::thread> threads_;
 };
 
 

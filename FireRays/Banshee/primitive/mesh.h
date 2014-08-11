33b5df16467625e0941a3079d160bb890990a1f5
yozhijk <dmitry.a.kozlov@gmail.com>
2014-08-11 11:20:59 +0400

    Start triangle mesh implementation

---
+++ FireRays/Banshee/primitive/mesh.h
@@ -0,0 +1,15 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include "transformable_primitive.h"
+#include "indexed_triangle.h"
+
+///< Transformable primitive implementation which represents
+///< triangle mesh.
+///<
+class Mesh : public TransformablePrimitive
+{
+    
+};
+
+#endif // MESH_H
